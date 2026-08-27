558127cc8e0995b44c9b5933f55530d3d027e2a9
jotap <jpleite948@gmail.com>
2026-08-27 11:37:35 -0300

    Chamados sendo exibidos no Dashboard|ADMIN | by jp

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const login = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:3000/login", {
+    const response = await fetch("http://localhost:3000/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -12,7 +12,7 @@ const Register = () => {
   const register = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:3000/users", {
+    const response = await fetch("http://localhost:3000/auth/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
